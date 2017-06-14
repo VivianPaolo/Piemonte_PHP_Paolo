3a97fd7294853bea1ecd0eedf1b02f9f3986e244
albertocaccaro001 <caccaroalberto001@gmail.com>
2017-06-14 09:02:34 +0200

    piemonte_php

--- lago-maggiore.php
+++ lago-maggiore.php
@@ -1,46 +1,32 @@
-<DOCTYPE html>
-<html lang="eng">
-	<head>
-	<?php
-		include_once "header.php"; // this will include header.php
-	?>
-		<!-- Mete tag, per la grandezza della pagina rispetto alla grandezza del dispositivo su cui stiamo guardando-->
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" contant="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Lago Maggiore</title>
-
-		<!-- File Bootstrap, ossia l'aspetto grafico del sito in generale-->
-		<!--BootStrap-->
-		<link href="css/vendor/bootstrap.min.css" rel="stylesheet">
-
-		<!--Supporto per IE8 e anche per elementi del HTML 5 e vari media-->
-		<!--Attenzione il file Respond.js se accediamo via file://-->
-		<!--se se vuole usare anche su IE 9
-	 	<script src="js/vendor/html5shiv-3.7.2.min.js"-->
-
-		<link href="css/custom.css" rel="stylesheet">
-		<title> Piemonte </title>
-	</head>
-	<body>
-
-		<p> Il <b> lago Maggiore </b> è un lago prealpino di origine fluvioglaciale, il secondo per superficie in Italia. Il suo nome "Maggiore" deriva dal fatto che un tempo si riteneva il più esteso dei laghi prealpini. </p>
-		<p> Il lago Maggiore si trova ad un'altezza di circa 193 m s.l.m., la sua superficie è di 212 km2 di cui circa l'80% è situata in territorio italiano e il rimanente 20% in territorio svizzero. Ha un perimetro di 170 km e una lunghezza di 64,37 km (la maggiore tra i laghi italiani); la larghezza massima è di 10 km e quella media di 3,9 km. </p>
-		<img src="images/lago-maggiore.jpg"><br>Il lago visto da sopra Brissago (Svizzera) 
-		<p><h2> Clima e statistiche </h2></p>
-		<p> Il Lago Maggiore è caratterizzato da inverni freddi, ma più miti rispetto all'entroterra, e moderatamente nevosi (con accumuli medi di 10 cm per ogni nevicata e talvolta anche superiori ai 30 cm fino ad un massimo di 50 cm), le estati sono moderatamente calde umide e temporalesche, la temperatura media di gennaio si attesta sui 2 gradi centigradi, con punte di 3 gradi sul versante settentrionale del golfo Borromeo, le temperature notturne possono scendere sotto lo 0, fino a -10, ma molto raramente scendere sotto tale valore. In estate le temperature medie si attestano attorno ai 22 gradi centigradi, con punte diurne che raramente superano i 32 gradi. </p>	
-		<p><h2> Luoghi </h2></p>
-		<p> Tra i luoghi caratteristici ricordiamo il <b> Mottarone </b> , conosciuto anche come "Montagna dei due laghi" per la sua particolare posizione tra Lago d'Orta e Lago Maggiore, è il più alto rilievo del Vergante. Dalla sua vetta, che in inverno si trasforma in una frequentata stazione sciistica, si gode uno dei più bei panorami sulle Alpi centro occidentali, sulla Pianura lombardo-piemontese e su sette laghi (Maggiore, Orta, Mergozzo, Varese, Comabbio, Monate e Biandronno). </p>	
-		<img src="images/mottarone.jpg"><br>La vetta del Mottarone 
-		<p> Il Lago Maggiore dista circa 35 km dal <a href="novara.html"> centro storico di Novara </a> e circa 50 km da Vercelli. </p>	
-		<hr>
-
-		<!--JQuery, necessario per il Bootstrap plugins-->
-
-		<script src="js/vendor/jquery-1.11.3.min.js"></script>
-
-		<!--Include tutti i plugin compilati o includono i file singoli di cui abbiamo bisogno-->
-
-		<scrypt src="js/vendor/bootstrap.min.js"></scrypt>
-	</body>
-</html>
+<?php
+	$title = "Lago Maggiore";
+	include_once "header_2.php";
+?>					
+          	<div id="main" class="col-md-offset-4 hidden-xs">
+          		<section>
+            		<h1>Il lago Maggiore</h1>
+            		<p>Il<em> lago Maggiore</em> è un lago prealpino di origine fluvioglaciale, il secondo per superficie in Italia. Il suo nome
+            		"Maggiore" deriva dal fatto che un tempo si riteneva il più esteso dei laghi prealpini.</p>
+            		<p>Il lago Maggiore si trova ad un'altezza di circa 193 m s.l.m., la sua superficie è di 212 km2 di cui circa l'80%<br/>è situata in territorio italiano e il rimanente 20% in territorio svizzero. Ha un perimetro di 170 km e una<br/>lunghezza di 64,37 km (la maggiore tra i laghi italiani); la larghezza massima è di<br/>10 km e quella media di 3,9 km.</p>
+            		<img src="images/lago-maggiore.jpg"/>
+            		<p>Il lago visto da sopra Brissago (Svizzera)</p>
+            		<h2>Clima e statistiche</h2>
+            		<p>Il Lago Maggiore è caratterizzato da inverni freddi, ma più miti rispetto all'entroterra, e moderatamente<br/>nevosi (con accumuli medi di 10 cm per ogni nevicata e talvolta anche superiori ai 30 cm fino ad un<br/>massimo di 50 cm), le estati sono moderatamente calde umide e temporalesche, la temperatura media di<br/>gennaio si attesta sui 2 gradi centigradi, con punte di 3 gradi sul versante settentrionale del golfo Borromeo,<br/>le temperature notturne possono scendere sotto lo 0, fino a -10, ma molto raramente scendere sotto tale<br/>valore. In estate le temperature medie si attestano attorno ai 22 gradi centigradi, con punte diurne che<br/>raramente superano i 32 gradi.</p>
+            		<h2>Luoghi</h2>
+            		<p>Tra i luoghi caratteristici ricordiamo il
+            		<strong>Mottarone</strong>
+            		, conosciuto anche come "Montagna dei due laghi" per la<br/>sua particolare posizione tra Lago d'Orta e Lago Maggiore, è il più alto rilievo del Vergante. Dalla sua vetta,<br/>che in inverno si trasforma in una frequentata stazione sciistica, si gode uno dei più bei panorami sulle 
+            		<a href="le-alpi.php">Alpi</a>
+            		,<br/>centro occidentali, sulla Pianura lombardo-piemontese e su sette laghi (Maggiore, Orta, Mergozzo, Varese,<br/>Comabbio, Monate e Biandronno).</p>
+            		<img src="images/mottarone.jpg"/>
+            		<p>La vetta del Mottarone</p>
+            		<p>Il Lago Maggiore dista circa 35 km dal
+            		<a href="centro-storico.php">centro storico di Novara </a>
+            		e circa 50 km da Vercelli.</p>
+            		<hr/>
+            		<a href="index.php">Torna alla pagina principale</a>
+          	  	</section>
+            </div>
+<?php
+	include_once "footer_2.php";
+?>
